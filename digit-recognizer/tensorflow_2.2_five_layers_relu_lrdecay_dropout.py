17280af008b66c79c1ca0f82f90346834ca87b1e
geekerlw <geekerlw@gmail.com>
2017-03-23 09:45:24 +0800

    five layers relu with lrdecay and dropout

---
+++ digit-recognizer/tensorflow_2.2_five_layers_relu_lrdecay_dropout.py
@@ -0,0 +1,136 @@
+# encoding: UTF-8
+# Copyright 2016 Google.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import math
+import input_data
+
+mnist = input_data.read_data_sets(one_hot=True)
+
+# neural network with 5 layers
+#
+# · · · · · · · · · ·          (input data, flattened pixels)       X [batch, 784]   # 784 = 28*28
+# \x/x\x/x\x/x\x/x\x/ ✞     -- fully connected layer (relu+dropout) W1 [784, 200]      B1[200]
+#  · · · · · · · · ·                                                Y1 [batch, 200]
+#   \x/x\x/x\x/x\x/ ✞       -- fully connected layer (relu+dropout) W2 [200, 100]      B2[100]
+#    · · · · · · ·                                                  Y2 [batch, 100]
+#     \x/x\x/x\x/ ✞         -- fully connected layer (relu+dropout) W3 [100, 60]       B3[60]
+#      · · · · ·                                                    Y3 [batch, 60]
+#       \x/x\x/ ✞           -- fully connected layer (relu+dropout) W4 [60, 30]        B4[30]
+#        · · ·                                                      Y4 [batch, 30]
+#         \x/               -- fully connected layer (softmax)      W5 [30, 10]        B5[10]
+#          ·                                                        Y5 [batch, 10]
+
+# Download images and labels into mnist.test (10K images+labels) and mnist.train (60K images+labels)
+# mnist = read_data_sets("data", one_hot=True, reshape=False, validation_size=0)
+
+# input X: 28x28 grayscale images, the first dimension (None) will index the images in the mini-batch
+X = tf.placeholder(tf.float32, [None, 784])
+# correct answers will go here
+Y_ = tf.placeholder(tf.float32, [None, 10])
+# variable learning rate
+lr = tf.placeholder(tf.float32)
+# Probability of keeping a node during dropout = 1.0 at test time (no dropout) and 0.75 at training time
+pkeep = tf.placeholder(tf.float32)
+
+# five layers and their number of neurons (tha last layer has 10 softmax neurons)
+L = 200
+M = 100
+N = 60
+O = 30
+# Weights initialised with small random values between -0.2 and +0.2
+# When using RELUs, make sure biases are initialised with small *positive* values for example 0.1 = tf.ones([K])/10
+W1 = tf.Variable(tf.truncated_normal([784, L], stddev=0.1))  # 784 = 28 * 28
+B1 = tf.Variable(tf.ones([L])/10)
+W2 = tf.Variable(tf.truncated_normal([L, M], stddev=0.1))
+B2 = tf.Variable(tf.ones([M])/10)
+W3 = tf.Variable(tf.truncated_normal([M, N], stddev=0.1))
+B3 = tf.Variable(tf.ones([N])/10)
+W4 = tf.Variable(tf.truncated_normal([N, O], stddev=0.1))
+B4 = tf.Variable(tf.ones([O])/10)
+W5 = tf.Variable(tf.truncated_normal([O, 10], stddev=0.1))
+B5 = tf.Variable(tf.zeros([10]))
+
+# The model, with dropout at each layer
+# XX = tf.reshape(X, [-1, 28*28])
+
+Y1 = tf.nn.relu(tf.matmul(X, W1) + B1)
+Y1d = tf.nn.dropout(Y1, pkeep)
+
+Y2 = tf.nn.relu(tf.matmul(Y1d, W2) + B2)
+Y2d = tf.nn.dropout(Y2, pkeep)
+
+Y3 = tf.nn.relu(tf.matmul(Y2d, W3) + B3)
+Y3d = tf.nn.dropout(Y3, pkeep)
+
+Y4 = tf.nn.relu(tf.matmul(Y3d, W4) + B4)
+Y4d = tf.nn.dropout(Y4, pkeep)
+
+Ylogits = tf.matmul(Y4d, W5) + B5
+Y = tf.nn.softmax(Ylogits)
+
+# cross-entropy loss function (= -sum(Y_i * log(Yi)) ), normalised for batches of 100  images
+# TensorFlow provides the softmax_cross_entropy_with_logits function to avoid numerical stability
+# problems with log(0) which is NaN
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=Ylogits, labels=Y_)
+cross_entropy = tf.reduce_mean(cross_entropy)*100
+
+# accuracy of the trained model, between 0 (worst) and 1 (best)
+correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# training step, the learning rate is a placeholder
+train_step = tf.train.AdamOptimizer(lr).minimize(cross_entropy)
+
+
+init = tf.global_variables_initializer()
+#sess = tf.Session()
+sess = tf.InteractiveSession()
+sess.run(init)
+
+for i in range(20000):
+	batch_x, batch_y = mnist.train.next_batch(100)
+	# learning rate decay
+	max_learning_rate = 0.003
+	min_learning_rate = 0.0001
+	decay_speed = 2000.0 # 0.003-0.0001-2000=>0.9826 done in 5000 iterations
+	learning_rate = min_learning_rate + (max_learning_rate - min_learning_rate) * math.exp(-i/decay_speed)
+	
+	if i % 100 == 0:
+		accuracy_step = sess.run(accuracy, feed_dict={X: mnist.validation.images, Y_: mnist.validation.labels, pkeep: 1.0})
+		print ("train_step => %d" % i)
+		print ("accuracy in this step is: %g" % accuracy_step)
+	sess.run(train_step, feed_dict={X: batch_x, Y_: batch_y, pkeep: 0.75, lr: learning_rate})
+	
+data = pd.read_csv("./data/test.csv")
+data = data.astype(np.float32)
+test_data = np.multiply(data, 1.0 / 255.0)
+
+predict = tf.argmax(Y, 1)
+predicted_labels = np.zeros(test_data.shape[0])
+for i in range(0, test_data.shape[0]//100):
+	predicted_labels[i*100: (i+1)*100] = predict.eval(feed_dict={X: test_data[i*100 : (i+1)*100], pkeep: 1.0})
+
+
+np.savetxt('submission.csv',
+			np.c_[range(1, len(test_data)+1), predicted_labels],
+			delimiter = ',',
+			header = 'ImageId,Label',
+			comments = '',
+			fmt = '%d')
+
+sess.close()
